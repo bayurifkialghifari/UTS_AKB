ce9d8882347d3f5bf284ae523b80e813cebc331f
bayurifkialghifari <bayurifkialgh@gmail.com>
2023-05-26 20:26:17 +0700

    fix insert query and table structure

--- app/src/main/java/com/uts/bayurifkialghifari_10120003/MyDBHelper.java
+++ app/src/main/java/com/uts/bayurifkialghifari_10120003/MyDBHelper.java
@@ -9,15 +9,19 @@
 
 package com.uts.bayurifkialghifari_10120003;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MyDBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "uts";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 3;
     private static final String TABLE_NAME = "notes";
     private static final String KEY_ID = "id";
     private static final String KEY_TITLE = "title";
@@ -26,26 +30,34 @@ public class MyDBHelper extends SQLiteOpenHelper {
     private static final String UPDATED_AT = "updated_at";
 
     public MyDBHelper(@Nullable Context context) {
-        super(context, this.DATABASE_NAME, null, this.DATABASE_VERSION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + this.TABLE_NAME + "("+ this.KEY_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
-                this.KEY_TITLE + " TEXT," +
-                this.KEY_DESCRIPTION +" TEXT," +
-                this.CREATED_AT + "TEXT," +
-                this.UPDATED_AT + "TEXT)");
-
-        SQLiteDatabase database = this.getWritableDatabase();
-
-        database.close();
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "("+ KEY_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                KEY_TITLE + " TEXT," +
+                KEY_DESCRIPTION +" TEXT," +
+                CREATED_AT + " datetime default current_timestamp," +
+                UPDATED_AT + " datetime default current_timestamp)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + this.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
         onCreate(db);
+    }
+
+    public void insertNote(String title, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = sdf.format(new Date());
+
+        ContentValues val = new ContentValues();
+        val.put(KEY_TITLE, title);
+        val.put(KEY_DESCRIPTION, description);
+
+        db.insert(TABLE_NAME, null, val);
     }
 }
